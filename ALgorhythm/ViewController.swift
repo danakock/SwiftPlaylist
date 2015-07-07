a973a9e314741c59b44162a2cd2a943d814b0321
Dana Kock <dana.kock@DANA-KOCK-MBP.local>
2015-07-07 15:52:37 -0700

    Initial Commit

---
+++ ALgorhythm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ALgorhythm
+//
+//  Created by Dana Kock on 7/7/15.
+//  Copyright (c) 2015 Tarman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
